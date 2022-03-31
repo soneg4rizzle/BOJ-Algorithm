11101004063c8f5574518b654e6cbad6e6b4dd3d
Seonggwan Han <gkstjdrhks30@gmail.com>
2022-03-31 15:36:50 +0900

    .

--- boj_11047/code.java
+++ boj_11047/code.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class code {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    List<Integer> list = new ArrayList<>();
+    List<Integer> list = new ArrayList<>(); // 동전의 종류를 저장할 리스트
     int cnt = 0; // 사용한 동전의 개수
-    int kind = sc.nextInt(); // 동전 N 종류
-    int K = sc.nextInt(); // N종류 동전으로 만들어야하는 값
+    int kind = sc.nextInt(); // N 종류의 동전
+    int K = sc.nextInt(); // N 종류의 동전으로 만들어야하는 값
     for (int i = 0; i < kind; i++)
       list.add(sc.nextInt()); // 동전의 종류를 모두 list에 저장.
 
-    int index = list.size() - 1;
+    int index = list.size() - 1; // 가장 큰 값의 동전부터 시작
     while (K != 0) { // 가치의 합이 0이 될때까지 가치의합-동전의종류 실행
       if (list.get(index) > K) {
         index--;
